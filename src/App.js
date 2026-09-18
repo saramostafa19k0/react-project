46b8b75b59c41b1cd4bdeb09681d1827b6a80875
saramostafa19k0 <sk8983639@gmail.com>
2026-09-18 20:50:56 +0300

    new feature

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@
 import AboutLittleLemon from './AboutLittleLemon';
 import { Routes, Route, Link } from "react-router-dom"; */
 
-import logo from './assets/logo copy.svg';
-
+//import logo from './assets/logo copy.svg';
+import React from 'react';
 import './App.css';
 /*import { useState } from 'react';
 
@@ -13,26 +13,33 @@ import LogoutButton from './LogoutButton';
 import HomePage from './Homepage1';
  */
 
+
 function App() {
 
-/*   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const login = () => setIsLoggedIn(true);
-  const logout = () => setIsLoggedIn(false); */
-
-  return (
- /*    <div>
-      {
-        isLoggedIn ? (
-          <div>
-            <HomePage />
-            <LogoutButton logout={logout} />
-          </div>
-        ) : (
-          <LoginButton login={login} />
-        )
-      }
-    </div> */
-    <img src={logo} className="App-logo" alt="logo" />
+const bird1 = new Audio("/bird1.mp3"); 
+  const bird2 = new Audio("/bird2.mp3"); 
+ 
+  function toggle1() { 
+    if (bird1.paused) { 
+      bird1.play(); 
+    } else { 
+      bird1.pause(); 
+    } 
+  }; 
+ 
+  function toggle2() { 
+    if (bird2.paused) { 
+      bird2.play(); 
+    } else { 
+      bird2.pause(); 
+    } 
+  }; 
+ 
+  return ( 
+    <div> 
+      <button onClick={toggle1}>Caspian Tern 1</button> 
+      <button onClick={toggle2}>Caspian Tern 2</button> 
+    </div> 
   );
 
    
